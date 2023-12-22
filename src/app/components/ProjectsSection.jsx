3f682dfdc797e8ad002a9a8574fdf73f4406672e
aniketsutrawe <aniketsutrawe28@gmail.com>
2023-12-22 21:44:07 +0530

    first commit

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,140 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+import Image from "next/image";
+import sparkles from "../../../public/icons/sparkles.png"
+
+
+
+// const projectsData = [
+//   {
+//     id: 1,
+//     title: "React Portfolio Website",
+//     description: "Project 1 description",
+//     image: "/images/projects/1.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 2,
+//     title: "Potography Portfolio Website",
+//     description: "Project 2 description",
+//     image: "/images/projects/2.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 3,
+//     title: "E-commerce Application",
+//     description: "Project 3 description",
+//     image: "/images/projects/3.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 4,
+//     title: "Food Ordering Application",
+//     description: "Project 4 description",
+//     image: "/images/projects/4.png",
+//     tag: ["All", "Mobile"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 5,
+//     title: "React Firebase Template",
+//     description: "Authentication and CRUD operations",
+//     image: "/images/projects/5.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 6,
+//     title: "Full-stack Roadmap",
+//     description: "Project 5 description",
+//     image: "/images/projects/6.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+// ];
+
+const ProjectsSection = () => {
+//   const [tag, setTag] = useState("All");
+//   const ref = useRef(null);
+//   const isInView = useInView(ref, { once: true });
+
+//   const handleTagChange = (newTag) => {
+//     setTag(newTag);
+//   };
+
+//   const filteredProjects = projectsData.filter((project) =>
+//     project.tag.includes(tag)
+//   );
+
+//   const cardVariants = {
+//     initial: { y: 50, opacity: 0 },
+//     animate: { y: 0, opacity: 1 },
+//   };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12 py-10">
+        My Projects
+      </h2>
+
+      <div className="lg:flex flex-row justify-center italic lg:text-2xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-secondary-500">
+        This is My first project<b>‎ The Portfolio</b> 
+        <br />
+        Next Project is under development and I can't wait to show you. <Image src={sparkles} className=" lg:w-7 lg:h-7 lg:mt-[28px] lg:ml-[6px] max-sm:inline-block max-sm:w-6"></Image>
+      </div>
+      
+
+      {/* <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul> */}
+    </section>
+  );
+};
+
+export default ProjectsSection;
