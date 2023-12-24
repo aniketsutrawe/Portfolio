85a2596f96874095b7b28190c82b2a3e8c278f53
aniketsutrawe <aniketsutrawe28@gmail.com>
2023-12-24 16:43:29 +0530

    with new resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1-D2Q7TNb8mUmqLY_wtRXy1WNiUivTx8v/view?usp=drive_link" target="blank"
+              href="https://drive.google.com/file/d/1i4iaufgbeTBW4-QoK6kagT3FtwJsjvzL/view?usp=sharing" target="blank"
               className="px-1 inline-block max-sm:text-center py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
