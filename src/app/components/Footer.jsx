38b2eed8fe7a23e7cdd424ebdfe4ae1e047314cf
aniketsutrawe <aniketsutrawe28@gmail.com>
2023-12-30 14:20:29 +0530

    Footer Container class removed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between font-logo text-xl">
+      <div className="p-12 flex justify-between font-logo text-xl">
         <span>Aniket Sutrawe</span>
         <p className=" text-primary-400 ">All rights reserved.</p>
       </div>
