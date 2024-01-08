9af984959be716ad071791e123c6a638017fbbc2
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-01-08 21:43:25 +0530

    Testing without api/send.js

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -26,7 +26,7 @@ const EmailSection = () => {
       message: e.target.message.value,
     };
     const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+    const endpoint = "https://formspree.io/f/xqkrknlv";
 
     // Form the request for sending data to the server.
     const options = {
